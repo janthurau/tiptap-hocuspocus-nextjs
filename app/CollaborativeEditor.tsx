b31329fa52cba4d20110cbbeab75c817aed5d114
Jan Thurau <jan@janthurau.de>
2024-01-05 16:29:50 +0100

    updates provider, adds multiplexing

--- app/CollaborativeEditor.tsx
+++ app/CollaborativeEditor.tsx
@@ -1,19 +1,29 @@
 "use client";
 
 import {useState, useRef, useEffect} from "react";
-import {HocuspocusProvider, WebSocketStatus} from "@hocuspocus/provider";
+import {
+  HocuspocusProvider,
+  WebSocketStatus,
+  HocuspocusProviderWebsocket,
+  TiptapCollabProvider, TiptapCollabProviderWebsocket
+} from "@hocuspocus/provider";
 import EditorComponent from "@/app/Editor";
 
 export default function CollaborativeEditorComponent(props: { documentName: string }) {
 
   const [status, setStatus] = useState(WebSocketStatus.Disconnected)
 
-  const provider = useRef<HocuspocusProvider>()
+  const provider = useRef<TiptapCollabProvider>()
+  const providerSocket = useRef<TiptapCollabProviderWebsocket>()
 
   useEffect(() => {
     if (typeof window !== undefined) {
-      provider.current = new HocuspocusProvider({
-        url: 'ws://127.0.0.1:8080',
+      providerSocket.current = new TiptapCollabProviderWebsocket({
+       appId: 'XY9DJ9E6',
+      })
+
+      provider.current = new TiptapCollabProvider({
+        websocketProvider: providerSocket.current,
         name: props.documentName,
         token: 'YOUR_TEST',
         onStatus(data) {
